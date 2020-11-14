2b0ed7e6c53a5daf214ca3649dd8a8595820580b
Rikin <dolllyamar@gmail.com>
2020-11-13 20:45:01 -0500

    Properly implemented dV_cloth_gravity_dq

--- src/dV_cloth_gravity_dq.cpp
+++ src/dV_cloth_gravity_dq.cpp
@@ -1,5 +1,9 @@
 #include <dV_cloth_gravity_dq.h>
 
 void dV_cloth_gravity_dq(Eigen::VectorXd &fg, Eigen::SparseMatrixd &M, Eigen::Ref<const Eigen::Vector3d> g) {
-    fg = -M * g;
+    Eigen::VectorXd global_g(M.rows());
+    for (int i = 0; i * 3 < M.rows(); i++) {
+        global_g.segment(i * 3, 3) = g;
+    }
+    fg = -M * global_g;
 }
