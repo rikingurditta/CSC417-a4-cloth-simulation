b8fdb7858272e6175a151f7ba8c72a5f4a6c809d
Rikin <dolllyamar@gmail.com>
2020-11-13 20:17:54 -0500

    Implemented collision_detection_cloth_sphere

--- src/collision_detection_cloth_sphere.cpp
+++ src/collision_detection_cloth_sphere.cpp
@@ -1,9 +1,16 @@
 #include <collision_detection_cloth_sphere.h>
 #include <iostream>
-void collision_detection_cloth_sphere(std::vector<unsigned int> &cloth_index, std::vector<Eigen::Vector3d> &normals, Eigen::Ref<const Eigen::VectorXd> q, Eigen::Ref<const Eigen::Vector3d> center, double radius) {
 
+void collision_detection_cloth_sphere(std::vector<unsigned int> &cloth_index, std::vector<Eigen::Vector3d> &normals,
+                                      Eigen::Ref<const Eigen::VectorXd> q, Eigen::Ref<const Eigen::Vector3d> center,
+                                      double radius) {
     cloth_index.clear();
     normals.clear();
-
-    
+    for (int i = 0; i * 3 < q.rows(); i++) {
+        Eigen::Vector3d d = q.segment(i * 3, 3) - center;
+        if (d.squaredNorm() < radius * radius) {
+            cloth_index.emplace_back(i);
+            normals.emplace_back(d.normalized());
+        }
+    }
 }
