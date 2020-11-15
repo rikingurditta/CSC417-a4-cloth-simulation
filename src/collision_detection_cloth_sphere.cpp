247c3478858e2ab50a01964738a0be3f452ddeae
Rikin <dolllyamar@gmail.com>
2020-11-15 14:45:53 -0500

    Fixed collision detection and handling

--- src/collision_detection_cloth_sphere.cpp
+++ src/collision_detection_cloth_sphere.cpp
@@ -8,7 +8,7 @@ void collision_detection_cloth_sphere(std::vector<unsigned int> &cloth_index, st
     normals.clear();
     for (int i = 0; i * 3 < q.rows(); i++) {
         Eigen::Vector3d d = q.segment(i * 3, 3) - center;
-        if (d.squaredNorm() < radius * radius) {
+        if (d.squaredNorm() <= radius * radius) {
             cloth_index.emplace_back(i);
             normals.emplace_back(d.normalized());
         }
--- src/velocity_filter_cloth_sphere.cpp
+++ src/velocity_filter_cloth_sphere.cpp
@@ -3,7 +3,9 @@
 void velocity_filter_cloth_sphere(Eigen::VectorXd &qdot, const std::vector<unsigned int> &indices, 
                                   const std::vector<Eigen::Vector3d> &normals) {
     for (int i = 0; i < indices.size(); i++) {
-        Eigen::Vector3d curr_qdot = qdot.segment(i * 3, 3);
-        qdot.segment(i * 3, 3) = curr_qdot - normals[i] * normals[i].transpose() * curr_qdot;
+        Eigen::Vector3d curr_qdot = qdot.segment(indices[i] * 3, 3);
+        if (curr_qdot.dot(normals[i]) < 0) {
+            qdot.segment(indices[i] * 3, 3) = curr_qdot - normals[i] * normals[i].transpose() * curr_qdot;
+        }
     }
 }
